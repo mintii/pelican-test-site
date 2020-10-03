799c12ed9566b4f946a105dbd560e2b0d74f4fea
Brittney Braxton <brittneynbraxton@gmail.com>
2020-10-02 22:41:33 -0400

    Add https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'quizzical-bartik-f5e425.netlify.app'
+SITEURL = 'https://quizzical-bartik-f5e425.netlify.app'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
