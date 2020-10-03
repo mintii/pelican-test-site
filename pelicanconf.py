051ac2e7a8924414345ae293b0acb9bf8b94427b
Brittney Braxton <brittneynbraxton@gmail.com>
2020-10-03 16:40:49 -0400

    pop

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,6 +29,7 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
+THEME = "simple"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
